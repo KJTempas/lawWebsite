cfb88b6884a35bb0c1834f3b2d536af814eb64bd
KJTempas <fw6927mm@go.minneapolis.edu>
2020-02-13 12:02:55 -0600

    updated indexOld  and html pgs for multipage site

--- law.js
+++ law.js
@@ -7,6 +7,8 @@ document.querySelectorAll('.nav').forEach(function(el){  //select all w/ the cla
         let name = this.getAttribute('name')
         let content = document.querySelector('#' + name)//combine # + name to get the location to scroll to
         content.scrollIntoView(true)
+        let navContainerHeight = navigator.containerHeight
+        content.scrollBy(0, -navContainerHeight)
         event.preventDefault()  //stop click event being folowed by browser
     })
 })
