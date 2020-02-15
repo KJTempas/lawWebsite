fb2e9b01ece0f6b5ccfc01666b36ac6c2c63fc1b
KJTempas <fw6927mm@go.minneapolis.edu>
2020-02-15 09:51:24 -0600

    updates to indexOld and linked pages

--- law.js
+++ law.js
@@ -4,11 +4,15 @@
 document.querySelectorAll('.nav').forEach(function(el){  //select all w/ the class="nav"
 
     el.addEventListener('click', function(event) {
-        let name = this.getAttribute('name')
-        let content = document.querySelector('#' + name)//combine # + name to get the location to scroll to
+        let name = this.getAttribute('name') //get the name attribute corresponding to the clicked item
+        //let content = document.querySelector('#' + name)//combine # + name to get the location to scroll to
+        let content = document.getElementById(name) //get content associated with name associated with clicked element
         content.scrollIntoView(true)
-        let navContainerHeight = navigator.containerHeight
-        content.scrollBy(0, -navContainerHeight)
+        let navContainer = document.querySelector('#nav-container')
+        let navContainerHeight = navContainer.clientHeight
+        console.log(navContainerHeight) //nothing showing
+        window.scrollBy(0, -navContainerHeight)  //(x, y values to scroll by)
+        //content.scrollBy(0, -navContainerHeight)
         event.preventDefault()  //stop click event being folowed by browser
     })
 })
